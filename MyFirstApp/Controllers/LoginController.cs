cca5192f637bbbdcc31bc9ed6dd3346b2d0b840e
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-12-13 10:28:26 -0900

    add new login controller

---
+++ MyFirstApp/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstApp.Models;
+
+namespace MyFirstApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.UserName == "BillGates" && userModel.Password == "bigbucks")
+            {
+                return View("LoginSuccess", userModel);
+            } else
+            {
+                View("LoginFailure", userModel);
+            }
+
+        }
+    }
+}
---
+++ MyFirstApp/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MyFirstApp.Models
+{
+	public class UserModel
+	{
+		public int Id { get; set; } = default!;
+
+		public string UserName { get; set; } = default!;
+
+		public string Password { get; set; } = default!;
+
+    }
+}
+
