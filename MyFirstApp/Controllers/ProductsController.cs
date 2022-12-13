a15520b87706184dfe4ff68a1d42880fbd206ac1
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-12-12 18:47:40 -0900

    add new controller

---
+++ MyFirstApp/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Message()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int secretNumber=13)
+        {
+            ViewBag.Name = name;
+            ViewBag.Secret = secretNumber;
+            return View();
+        }
+    }
+}
